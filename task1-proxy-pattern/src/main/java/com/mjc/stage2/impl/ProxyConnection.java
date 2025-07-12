087de5b3ca714fe65ebef8b1fb9d05afbdaacd60
Fwalt237 <patrickkamwa17@gmail.com>
2025-07-12 15:48:30 +0100

    pc

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,7 +1,10 @@
 package com.mjc.stage2.impl;
+import com.mjc.stage2.Connection;
+
+import java.sql.DriverManager;
 
 
-public class ProxyConnection {
+public class ProxyConnection implements Connection {
     private RealConnection realConnection;
 
     public ProxyConnection(RealConnection realConnection) {
@@ -10,6 +13,19 @@ public class ProxyConnection {
 
     public void reallyClose() {
         // Write your code here!
+        Connection proxyConnection=realConnection;
+        realConnection=null;
+        proxyConnection.close();
+    }
+
+    @Override
+    public void close() {
+        this.realConnection.close();
+    }
+
+    @Override
+    public boolean isClosed() {
+        return this.realConnection.isClosed();
     }
     // Implement methods here!
 }
